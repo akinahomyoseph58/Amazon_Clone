7c0a80419e1c72ce235328a19d4aab702b06ed92
Akinahom Yoseph <akinahomyoseph48-bit>
2026-02-02 22:42:48 +0300

    Created logout.php to destroy user session

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start(); // start session
+session_unset(); // remove all session variables
+session_destroy(); // destroy the session
+header("Location: signin.php"); // redirect to signin page
+exit();
+?>
